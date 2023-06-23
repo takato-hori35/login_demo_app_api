88ec836b4b1a2911174fd395260b5c07e49c22af
takato hori <iuyda@icloud.com>
2023-06-23 23:08:47 +0900

    add test endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+   # 追加
+   namespace :api do
+    namespace :v1 do
+      # api test action
+      resources :hello, only:[:index]
+    end
+  end
 end
